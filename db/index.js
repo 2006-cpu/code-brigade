24eb529bf950f7b170ec443cb3fd8e5f07281d95
tillyninjaspace <squealingpet@gmail.com>
2020-11-16 23:51:10 -0800

    feature(database-adapter): getAllProducts

--- db/index.js
+++ db/index.js
@@ -1,13 +1,29 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
+// const DB_NAME = 'graceshopper-dev'
+// grace-shopper
+const DB_NAME = 'localhost:5432/graceshopper-dev'
 const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
+async function getAllProducts() {
+
+  try {
+    const {rows: products} = await client.query(`
+      SELECT * 
+      FROM products
+    `);
+  
+    return products
+  } catch (error) {
+    throw error;
+  } 
+};
 
 // export
 module.exports = {
   client,
+  getAllProducts
   // db methods
 }
