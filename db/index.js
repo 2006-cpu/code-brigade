deba115f6a456f960baa3232fa3639b052e2e513
tillyninjaspace <squealingpet@gmail.com>
2020-11-17 16:54:26 -0800

    Combined Database Adapters: getProductById, getAllProducts, createProduct

--- db/index.js
+++ db/index.js
@@ -1,13 +1,64 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
+const DB_NAME = 'localhost:5432/graceshopper-dev'
 const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
+async function getProductById(id) {
+  try {
+    const { rows: [ product ]  } = await client.query(`
+          SELECT *
+          FROM products
+          WHERE id=$1;
+        `, [id]);
+    
+        return product
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+async function getAllProducts() {
+
+  try {
+    const {rows: products} = await client.query(`
+      SELECT * 
+      FROM products
+    `);
+
+    return products
+  } catch (error) {
+    throw error;
+  } 
+};
+
+
+async function createProduct(product) {
+
+  const { name, description, price, imageURL, inStock, category } = product
+  
+  try {
+   
+    const { rows: [ newProduct ] } = await client.query(`
+      INSERT INTO products(name, description, price, imageURL, inStock, category) 
+      VALUES($1, $2, $3, $4, $5, $6) 
+      RETURNING *;
+    `, [name, description, price, imageURL, inStock, category]);
+
+       return newProduct;
+
+  } catch (error) {
+    throw error;
+  }
+};
 
 // export
 module.exports = {
   client,
+  getProductById,
+  getAllProducts,
+  createProduct
   // db methods
 }
