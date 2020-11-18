ad1ba6009d30d2f8b830b9a337aaaa2daf5c8680
Zach Garrett <zacharytheodoregarrett@gmail.com>
2020-11-17 19:16:42 -0800

    added get products

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const apiRouter = require('express').Router();
+const { getAllProducts, getProductById } = require('../db');
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
@@ -6,7 +7,23 @@ apiRouter.get("/", (req, res, next) => {
   });
 });
 
-const productsRouter = require('./products');
-apiRouter.use('/products', productsRouter);
+apiRouter.get('/products', async (req, res, next) => {
+  try {
+      const products = await getAllProducts();
+      res.send(products);
+  } catch (error) {
+      next(error);
+  }
+})
+
+apiRouter.get('/product/:productId', async (req, res, next) => {
+  const id = req.params.productId;
+  try {
+      const productById = await getProductById(id);
+      res.send(productById);
+  } catch (error) {
+     next(error);
+  }
+})
 
 module.exports = apiRouter;
