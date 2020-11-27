6ef094c2fc818d440fd8b07298d386c80efbcd5d
tillyninjaspace <squealingpet@gmail.com>
2020-11-27 14:36:18 -0800

    removed some comments

--- db/index.js
+++ db/index.js
@@ -310,13 +310,6 @@ async function addProductToOrder({ orderId, productId, price, quantity }) {
 
     const newOrderProduct = await createOrderProductsList({ orderId, productId, price, quantity })
 
-    //   const { rows: [ orderProduct ] }  = await client.query(`
-    //   INSERT INTO order_products("productId", "orderId", price, quantity)
-    //   VALUES($1, $2, $3, $4)
-    //   RETURNING *;
-    // `, [ productId, orderId, price, quantity ])
-      // return orderProduct;
-
       return newOrderProduct
     } else {
 
