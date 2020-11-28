7db4bba5fcae11c1ebeecf506e05a1d23aa6b158
Your Name <your_email@real_email.edu>
2020-11-27 20:28:28 -0800

    moved /orders_products/:orderProductId route to order_products file

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const productsRouter = express.Router();
-const   { getAllProducts, updateOrderProduct } 
+const   { getAllProducts } 
         = require('../db/')
 
 productsRouter.get('/', async (req, res, next) => {
@@ -11,20 +11,5 @@ productsRouter.get('/', async (req, res, next) => {
         next(error);
     }
   })
-
-  productsRouter.patch('/orders_products/:orderProductId', async (req, res, next) => {
-
-    try {
-      await updateOrderProduct({
-          id: req.params.orderProductId, 
-          price: req.body.price,
-          quantity: req.body.quantity
-        });
-        res.send(204)
-
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  });
 
   module.exports = productsRouter;
