387016f3ee97c0237f7bd36d63c02601970fecec
Your Name <your_email@real_email.edu>
2020-12-03 14:08:03 -0800

    stripe updates

--- src/components/TakeMoney.js
+++ src/components/TakeMoney.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
-import {useHistory} from 'react-router-dom';
+// import {useHistory} from 'react-router-dom';
 import { getStripe, getAllProducts } from '../api/index.js'
 const STRIPE_API_KEY = process.env.REACT_APP_STRIPE_API_KEY
 console.log(STRIPE_API_KEY)
@@ -11,12 +11,12 @@ console.log(STRIPE_API_KEY)
 export default class TakeMoney extends React.Component {
   onToken = (token) => {
     console.log(token)
-    const history = useHistory();
+    // const history = useHistory();
     const stripe = async () => {
       try { 
-        if (token) {
-          history.push('/products')
-        }
+        // if (token) {
+        //   history.push('/products')
+        // }
        console.log(await getStripe(token))
       } catch (error) {
        throw error;
